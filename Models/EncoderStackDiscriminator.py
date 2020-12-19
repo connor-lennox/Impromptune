664c3fed269bdae3c5fa4d1f988b66f166e39443
Connor Lennox <clennox99@gmail.com>
2020-12-19 11:24:21 -0500

    Basic implementation of an Encoder Stack Discriminator

---
+++ Models/EncoderStackDiscriminator.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+
+from Discriminator import Discriminator
+
+
+class EncoderStackDiscriminator(Discriminator):
+    def __init__(self, d_model=128, n_head=8, num_layers=6):
+        super().__init__(d_model)
+
+        encoder_layer = nn.TransformerEncoderLayer(d_model, n_head)
+        self.encoder_stack = nn.TransformerEncoder(encoder_layer, num_layers)
+
+        # Linear layer for binary classification
+        self.linear = nn.Linear(d_model, 2)
+
+    def forward(self, src):
+        out = self.encoder_stack(src)
+        out = self.linear(out)
+        out = func.softmax(out, 2)
+        return out
+
+
+if __name__ == '__main__':
+    enc = EncoderStackDiscriminator(d_model=8)
+    noise = torch.randn((1, 1, 8))
+    result = enc(noise)
+    print(result)
