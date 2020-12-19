5c75bd7ae7e43d539cbf7512e0220e16a2263603
Connor Lennox <clennox99@gmail.com>
2020-12-19 10:43:06 -0500

    Slightly changed Generator interface - might change back later

--- Models/Generator.py
+++ Models/Generator.py
@@ -4,10 +4,9 @@ import torch.nn as nn
 class Generator(nn.Module):
     """A top-level Generator class, defining the first half of the GAN structure.
     Since exact implementations of generation can differ, very little is present here. However,
-    all Generators will have some amount of inputs and some amount of outputs.
+    all Generators will have some amount of input features.
     """
 
-    def __init__(self, input_shape: tuple, output_shape: tuple):
+    def __init__(self, d_model: int):
         super(Generator, self).__init__()
-        self.input_shape = input_shape
-        self.output_shape = output_shape
+        self.d_model = d_model
