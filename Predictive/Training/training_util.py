fea6d5e20992bdd2c2e8b53c24a643cf0b3dc22d
Connor Lennox <clennox99@gmail.com>
2020-12-31 10:35:18 -0500

    Additional function for accuracy calculation

--- Predictive/Training/training_util.py
+++ Predictive/Training/training_util.py
@@ -21,6 +21,12 @@ def create_train_test(samples, train_ratio=0.8, given=24):
     return x_train, y_train, x_test, y_test
 
 
+def accuracy(predicted, real):
+    predictions = np.argmax(predicted.detach().numpy(), axis=1)
+    real_np = real.detach().numpy()
+    return np.mean(predictions == real_np)
+
+
 if __name__ == '__main__':
     test_data = [[1, 2, 3, 4, 5, 6], [2, 4, 6, 8, 10]]
     print(data_from_samples(test_data, given=3))
