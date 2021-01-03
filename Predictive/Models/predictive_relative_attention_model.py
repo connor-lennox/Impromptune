b0c27d28247e1936c7e67d6443f62b417dc661f4
Connor Lennox <clennox99@gmail.com>
2021-01-03 17:51:08 -0500

    Implemented intermediary attention layers how they should be PRAmBlocks are an efficient relative multihead attention layer, followed by a feed-forward linear layer. Latent connections and layer normalizations present after each sublayer of the PRAmBlock.

--- Predictive/Models/predictive_relative_attention_model.py
+++ Predictive/Models/predictive_relative_attention_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from Predictive.Models.relative_multihead_attention import \
     PredictiveRelativeMultiheadAttention, EfficientRelativeMultiheadAttention
@@ -7,7 +8,7 @@ from Predictive.Models.one_hot_embedding import OneHotEmbedding
 
 
 class PRAm(nn.Module):
-    def __init__(self, embedding_dim=256, key_dim=64, use_onehot_embed=False, num_attn_layers=2):
+    def __init__(self, embedding_dim=256, key_dim=64, value_dim=256, use_onehot_embed=False, num_attn_layers=2):
         super().__init__()
 
         if use_onehot_embed:
@@ -16,10 +17,10 @@ class PRAm(nn.Module):
         else:
             self.embedding = nn.Embedding(num_embeddings=333, embedding_dim=embedding_dim)
 
-        self.rel_attn_layers = [EfficientRelativeMultiheadAttention(embedding_dim, key_dim, embedding_dim)
+        self.rel_attn_layers = [PRAmBlock(embedding_dim, key_dim, value_dim)
                                 for _ in range(num_attn_layers)]
-        self.pred_attn = PredictiveRelativeMultiheadAttention(embedding_dim, key_dim, embedding_dim)
-        self.linear = nn.Linear(embedding_dim, 333)
+        self.pred_attn = PredictiveRelativeMultiheadAttention(embedding_dim, key_dim, value_dim)
+        self.linear = nn.Linear(value_dim, 333)
 
     def forward(self, xs):
         xs = self.embedding(xs)
@@ -27,6 +28,24 @@ class PRAm(nn.Module):
             xs = rel_attn(xs)
         xs = self.pred_attn(xs)
         xs = self.linear(xs)
+        return xs
+
+
+class PRAmBlock(nn.Module):
+    def __init__(self, embedding_dim=256, key_dim=64, value_dim=256, activation=F.relu):
+        super().__init__()
+
+        self.attn = EfficientRelativeMultiheadAttention(embedding_dim, key_dim=key_dim, value_dim=value_dim)
+        self.norm1 = nn.LayerNorm(value_dim)
+        self.linear = nn.Linear(value_dim, embedding_dim)
+        self.norm2 = nn.LayerNorm(embedding_dim)
+        self.activation = activation
+
+    def forward(self, xs):
+        res = self.attn(xs)
+        xs = self.norm1(xs + res)
+        res = self.activation(self.linear(xs))
+        xs = self.norm2(xs + res)
         return xs
 
 
