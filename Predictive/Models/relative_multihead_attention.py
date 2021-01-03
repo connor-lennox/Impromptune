c90c9bb4bc7dba1d2c3afea6e19fffd43d1e4b6c
Connor Lennox <clennox99@gmail.com>
2021-01-02 21:14:34 -0500

    Added efficiently calculated relative positional encoding using skewing mechanism Doesn't currently do relative value calculations, just for query/key Hasn't been tested yet to ensure it's producing correct values

--- Predictive/Models/relative_multihead_attention.py
+++ Predictive/Models/relative_multihead_attention.py
@@ -47,6 +47,7 @@ class RelativeMultiheadAttention(nn.Module):
         values = torch.einsum('bse,hev->bhsv', xs, self.w_v)
 
         # queries * keys^T
+        # Here q is the "query/key" dimension, which are guaranteed to be the same length
         q_k = torch.einsum('bhiq,bhjq->bhij', queries, keys)
 
         # queries * relative key encoding
@@ -81,6 +82,98 @@ class RelativeMultiheadAttention(nn.Module):
         rel_matrix = torch.clamp(rel_matrix, -self.relative_cutoff, self.relative_cutoff)
         rel_matrix = rel_matrix + self.relative_cutoff
         return embeddings_matrix[rel_matrix]
+
+
+class EfficientRelativeMultiheadAttention(nn.Module):
+    """Improvements on Relative Multihead Attention as described in Music Transformer (Huang et. al., 2018)"""
+    def __init__(self, embed_dim, key_dim=64, value_dim=64, n_heads=8, relative_cutoff=128):
+        super().__init__()
+
+        self.value_dim = value_dim
+        # Relative cutoff in each direction
+        self.relative_cutoff = relative_cutoff
+
+        # Typical multi-head attention parameters
+        self.w_q = nn.Parameter(torch.Tensor(n_heads, embed_dim, key_dim))
+        self.w_k = nn.Parameter(torch.Tensor(n_heads, embed_dim, key_dim))
+        self.w_v = nn.Parameter(torch.Tensor(n_heads, embed_dim, value_dim))
+        self.w_o = nn.Parameter(torch.Tensor(n_heads))
+
+        # Relative positional values for each element relative_cutoff out from center, plus one for center
+        self.a_k = nn.Parameter(torch.Tensor(relative_cutoff*2+1, key_dim))
+        self.a_v = nn.Parameter(torch.Tensor(relative_cutoff*2+1, value_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.w_q, a=sqrt(5))
+        init.kaiming_uniform_(self.w_k, a=sqrt(5))
+        init.kaiming_uniform_(self.w_v, a=sqrt(5))
+        init.ones_(self.w_o)
+
+        init.kaiming_uniform_(self.a_k, a=sqrt(5))
+        init.kaiming_uniform_(self.a_v, a=sqrt(5))
+
+    def forward(self, xs):
+        """xs of shape (batch, seq_len, embed_dim)"""
+        seq_len = xs.shape[1]
+
+        # Standard calculation of queries, keys, and values for each element of the input sequence
+        # Each has shape (batch, n_head, seq_len, result), where result is the query/key/value dimension
+        queries = torch.einsum('bse,heq->bhsq', xs, self.w_q)
+        keys = torch.einsum('bse,hek->bhsk', xs, self.w_k)
+        values = torch.einsum('bse,hev->bhsv', xs, self.w_v)
+
+        # queries * keys^T
+        # Here q is the "query/key" dimension, which are guaranteed to be the same length
+        q_k = torch.einsum('bhiq,bhjq->bhij', queries, keys)
+
+        # queries x relative positional encodings
+        # using "skewing" technique
+        # This matrix has shape (batch, n_heads, seq_len, seq_len*2+1), and the elements are not lined up properly.
+        q_rel = torch.einsum('bhiq,rq->bhir',
+                             queries, self._generate_relative_positional_embeddings(self.a_k, seq_len))
+        q_rel = self._skew_matrix(q_rel)
+
+        # sum the standard q*k component and the relative positional query component
+        # division by sqrt(value_dim) for normalization
+        e = (q_k + q_rel) / sqrt(self.value_dim)
+        alphas = F.softmax(e, dim=3)
+
+        # alphas * values (summed over j axis)
+        # qkv shape = (batch, heads, seq_len, value_dim)
+        qkv = torch.einsum('bhij,bhjv->bhiv', alphas, values)
+
+        # Temporarily dropping relative value calculation (is it required? Maybe not.)
+        # v_rel = torch.einsum('bhij,rv->bhir',
+        #                      alphas, self._generate_relative_positional_embeddings(self.a_v, seq_len))
+        #
+        # v_rel = self._skew_matrix(v_rel)
+
+        # z of shape (batch, n_head, seq_len, value_dim)
+        z = qkv  # + v_rel
+
+        # Combine heads with scaling factors in w_o
+        result = torch.einsum('bhsv,h->bsv', z, self.w_o)
+
+        # Final output shape: (batch, seq_len, value_dim)
+        return result
+
+    def _generate_relative_positional_embeddings(self, embedding_matrix, seq_len):
+        pos_vec = torch.arange(-seq_len+1, seq_len)
+        pos_vec = torch.clamp(pos_vec, -self.relative_cutoff, self.relative_cutoff)
+        pos_vec += self.relative_cutoff
+        return embedding_matrix[pos_vec]
+
+    @staticmethod
+    def _skew_matrix(matrix):
+        seq_len = matrix.shape[2]
+        matrix = F.pad(matrix, [0, 1])          # Pad one column on right side of data
+        matrix = torch.flatten(matrix, 2, 3)    # Flatten last two dimensions
+        matrix = F.pad(matrix, [0, seq_len-1])  # Pad an additional seq_len-1 elements
+        matrix = torch.reshape(matrix, (matrix.shape[0], matrix.shape[1], seq_len+1, 2*seq_len-1))  # Reshape matrix
+        matrix = matrix[:, :, :seq_len, matrix.shape[0]-seq_len-1:]   # Slice to retain first seq_len rows and last seq_len columns
+        return matrix
 
 
 class PredictiveRelativeMultiheadAttention(nn.Module):
@@ -158,10 +251,14 @@ class PredictiveRelativeMultiheadAttention(nn.Module):
 
 
 if __name__ == '__main__':
-    # parameter of (embed)
-    test_att = PredictiveRelativeMultiheadAttention(128, value_dim=128)
-    # input of shape (batch,seq,embed)
-    test_input = torch.randn((256, 1000, 128))
+    # # parameter of (embed)
+    # test_att = PredictiveRelativeMultiheadAttention(128, value_dim=128)
+    # # input of shape (batch,seq,embed)
+    # test_input = torch.randn((256, 1000, 128))
+    # test_result = test_att(test_input)
+    # # shape should be (batch,value_dim), as only one output is generated per input sequence
+    # print(test_result.shape)
+    test_att = EfficientRelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=8, relative_cutoff=1)
+    test_input = torch.randn((1, 3000, 3))
     test_result = test_att(test_input)
-    # shape should be (batch,value_dim), as only one output is generated per input sequence
     print(test_result.shape)
