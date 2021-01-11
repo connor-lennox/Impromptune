c47e93a8a043eecf67d004930845a022fa15cf9d
Connor Lennox <clennox99@gmail.com>
2021-01-10 20:06:01 -0500

    Added local relative attention module

--- Predictive/Models/relative_multihead_attention.py
+++ Predictive/Models/relative_multihead_attention.py
@@ -176,6 +176,98 @@ class EfficientRelativeMultiheadAttention(nn.Module):
         return matrix
 
 
+class LocalRelativeMultiheadAttention(nn.Module):
+    def __init__(self, embed_dim, key_dim=64, value_dim=64, n_heads=8, look_back=128, look_forward=0):
+        super().__init__()
+
+        self.value_dim = value_dim
+        self.look_back = look_back
+        self.look_forward = look_forward
+
+        # Standard attention parameters
+        self.w_q = nn.Parameter(torch.Tensor(n_heads, embed_dim, key_dim))
+        self.w_k = nn.Parameter(torch.Tensor(n_heads, embed_dim, key_dim))
+        self.w_v = nn.Parameter(torch.Tensor(n_heads, embed_dim, value_dim))
+        self.w_o = nn.Parameter(torch.Tensor(n_heads))
+
+        # Relative position parameter (applied only to queries) based on lookback/forward
+        self.a_k = nn.Parameter(torch.Tensor(look_back+1+look_forward, key_dim))
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        init.kaiming_uniform_(self.w_q, a=sqrt(5))
+        init.kaiming_uniform_(self.w_k, a=sqrt(5))
+        init.kaiming_uniform_(self.w_v, a=sqrt(5))
+        init.ones_(self.w_o)
+        init.kaiming_uniform_(self.a_k, a=sqrt(5))
+
+    def forward(self, xs):
+        """xs of shape (batch, seq_len, embed_dim)"""
+        seq_len = xs.shape[1]
+        local_mask = self._generate_local_mask(seq_len, xs.device)
+
+        # Standard calculation of queries, keys, and values for each element of the input sequence
+        # Each has shape (batch, n_head, seq_len, result), where result is the query/key/value dimension
+        queries = torch.einsum('bse,heq->bhsq', xs, self.w_q)
+        keys = torch.einsum('bse,hek->bhsk', xs, self.w_k)
+        values = torch.einsum('bse,hev->bhsv', xs, self.w_v)
+
+        # queries * keys^T
+        # Here q is the "query/key" dimension, which are guaranteed to be the same length
+        q_k = torch.einsum('bhiq,bhjq->bhij', queries, keys)
+
+        # queries x relative positional encodings
+        # using "skewing" technique
+        # This matrix has shape (batch, n_heads, seq_len, seq_len*2+1), and the elements are not lined up properly.
+        q_rel = torch.einsum('bhiq,rq->bhir',
+                             queries, self._generate_relative_positional_embeddings(self.a_k, seq_len))
+        q_rel = self._skew_matrix(q_rel)
+
+        # sum the standard q*k component and the relative positional query component
+        # division by sqrt(value_dim) for normalization
+        e = (q_k + q_rel) / sqrt(self.value_dim)
+        alphas = F.softmax(e, dim=3)
+
+        # Mask out "non-local" values for element compatibility
+        # Logically, you are completely incompatible with elements outside of your neighborhood
+        alphas = torch.einsum('bhij,ij->bhij', alphas, local_mask)
+
+        # alphas * values (summed over j axis)
+        # qkv shape = (batch, heads, seq_len, value_dim)
+        qkv = torch.einsum('bhij,bhjv->bhiv', alphas, values)
+
+        # Combine heads with scaling factors in w_o
+        result = torch.einsum('bhsv,h->bsv', qkv, self.w_o)
+
+        # Final output shape: (batch, seq_len, value_dim)
+        return result
+
+    def _generate_local_mask(self, seq_len, device):
+        mask = torch.tensor([0] + [1] * (self.look_back+1+self.look_forward) + [0] * (seq_len-1-self.look_back)).to(device)
+        mask = mask.repeat(seq_len)
+        mask = mask[(self.look_back+1):-(seq_len-1-self.look_back)]
+        mask = mask.reshape(seq_len, -1)
+        mask = mask[:seq_len, :seq_len]
+        return mask
+
+    def _generate_relative_positional_embeddings(self, embedding_matrix, seq_len):
+        pos_vec = torch.arange(-seq_len+1, seq_len)
+        pos_vec = torch.clamp(pos_vec, -self.look_back, self.look_forward)
+        pos_vec += self.look_back
+        return embedding_matrix[pos_vec]
+
+    @staticmethod
+    def _skew_matrix(matrix):
+        seq_len = matrix.shape[2]
+        matrix = F.pad(matrix, [0, 1])          # Pad one column on right side of data
+        matrix = torch.flatten(matrix, 2, 3)    # Flatten last two dimensions
+        matrix = F.pad(matrix, [0, seq_len-1])  # Pad an additional seq_len-1 elements
+        matrix = torch.reshape(matrix, (matrix.shape[0], matrix.shape[1], seq_len+1, 2*seq_len-1))  # Reshape matrix
+        matrix = matrix[:, :, :seq_len, matrix.shape[3]-seq_len:]   # Slice to retain first seq_len rows and last seq_len columns
+        return matrix
+
+
 class PredictiveRelativeMultiheadAttention(nn.Module):
     def __init__(self, embed_dim, key_dim=64, value_dim=64, n_heads=8, relative_cutoff=128):
         super().__init__()
@@ -258,13 +350,18 @@ if __name__ == '__main__':
     # test_result = test_att(test_input)
     # # shape should be (batch,value_dim), as only one output is generated per input sequence
     # print(test_result.shape)
-    test_eff_att = EfficientRelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=1, relative_cutoff=1)
-    test_att = RelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=1, relative_cutoff=1)
-    test_att.load_state_dict(test_eff_att.state_dict())
-    test_input = torch.randn((1, 3, 3))
-    # These results won't be the same since the efficient attention does not do relative positional values,
-    # but calculating both provides an entry to the function so that a breakpoint can look at the intermediary
-    # relative query position results.
-    test_result = test_att(test_input)
-    test_eff_result = test_eff_att(test_input)
-    print(test_result.shape)
+    # test_eff_att = EfficientRelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=1, relative_cutoff=1)
+    # test_att = RelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=1, relative_cutoff=1)
+    # test_att.load_state_dict(test_eff_att.state_dict())
+    # test_input = torch.randn((1, 3, 3))
+    # # These results won't be the same since the efficient attention does not do relative positional values,
+    # # but calculating both provides an entry to the function so that a breakpoint can look at the intermediary
+    # # relative query position results.
+    # test_result = test_att(test_input)
+    # test_eff_result = test_eff_att(test_input)
+    # print(test_result.shape)
+
+    test_local_attn = LocalRelativeMultiheadAttention(3, key_dim=4, value_dim=4, n_heads=8, look_back=2, look_forward=2)
+    test_input = torch.randn(8, 7, 3)
+    test_output = test_local_attn(test_input)
+    print(test_output.shape)
