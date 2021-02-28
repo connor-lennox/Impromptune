9059afa385e11568c63822c7de8d726e39c589ec
Connor Lennox <clennox99@gmail.com>
2021-02-28 10:49:12 -0500

    Alternative "locked" embedding that gets given a matrix and applies no learning

--- Predictive/Models/one_hot_embedding.py
+++ Predictive/Models/one_hot_embedding.py
@@ -9,3 +9,12 @@ class OneHotEmbedding(nn.Module):
 
     def forward(self, xs):
         return self.weights[xs]
+
+
+class LockedEmbedding(nn.Module):
+    def __init__(self, embedding_matrix):
+        super().__init__()
+        self.weights = torch.nn.Parameter(torch.Tensor(embedding_matrix), requires_grad=False)
+
+    def forward(self, xs):
+        return self.weights[xs]
